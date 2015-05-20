5c29ff05526f2d49904f9464e8bbfe318adf6020
Bruce Eckel <BruceTEckel@gmail.com>
2015-05-20 00:19:37 -0700

    Solved the new annotations problem!

---
+++ annotations/ifx/ExtractInterface.java
@@ -0,0 +1,10 @@
+//: annotations/ifx/ExtractInterface.java
+// javac-based annotation processing.
+package annotations.ifx;
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ExtractInterface {
+  public String interfaceName() default "-!!-";
+} ///:~
