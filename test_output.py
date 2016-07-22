873637cf1d73016426729c2a1d495852b735ea3e
Bruce Eckel <BruceTEckel@gmail.com>
2016-07-22 17:26:49 -0600

    Added "gradlew verify"

--- test_output.py
+++ test_output.py
@@ -5,10 +5,53 @@
 # (2) If direct comparison of actual output with output stored in Java file fails:
 # (3) Use chain of responsibility to successively try filters until one matches or all fail
 from pathlib import Path
+import textwrap
 import re
 
+def fill_to_width(text):
+    result = ""
+    for line in text.splitlines():
+        result += textwrap.fill(line, width=59) + "\n"
+    return result.strip()
+
+def adjust_lines(text):
+    text = text.replace("\0", "NUL")
+    lines = text.splitlines()
+    slug = lines[0]
+    if "(First and Last " in slug:
+        num_of_lines = int(slug.split()[5])
+        adjusted = lines[:num_of_lines + 1] +\
+            ["...________...________...________...________..."] +\
+            lines[-num_of_lines:]
+        return "\n".join(adjusted)
+    elif "(First " in slug:
+        num_of_lines = int(slug.split()[3])
+        adjusted = lines[:num_of_lines + 1] +\
+            ["                  ..."]
+        return "\n".join(adjusted)
+    else:
+        return text
+
+def phase1():
+    """
+    (0) Do first/last lines before formatting to width
+    (1) Combine output and error (if present) files
+    (2) Format all output to width limit
+    (3) Add closing '*/'
+    """
+    for outfile in Path(".").rglob("*.out"):
+        out_text = adjust_lines(outfile.read_text())
+        phase_1 = outfile.with_suffix(".p1")
+        with phase_1.open('w') as phs1:
+            phs1.write(fill_to_width(out_text) + "\n")
+            errfile = outfile.with_suffix(".err")
+            if errfile.exists():
+                phs1.write("___[ Error Output ]___\n")
+                phs1.write(fill_to_width(errfile.read_text()) + "\n")
+            phs1.write("*/\n")
 
 if __name__ == '__main__':
+    phase1()
     find_output = re.compile(r"/\* (Output:.*)\*/", re.DOTALL) # should space be \s+ ??
     for outfile in Path(".").rglob("*.p1"):
         javafile = outfile.with_suffix(".java")
