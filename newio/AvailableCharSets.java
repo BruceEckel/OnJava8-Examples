052deaba26067088f52df5149427ca528365af57
Bruce Eckel <BruceTEckel@gmail.com>
2017-01-20 10:58:00 -0800

    Test removal for CI problems

--- newio/AvailableCharSets.java
+++ newio/AvailableCharSets.java
@@ -3,6 +3,7 @@
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
 // Displays Charsets and aliases
+// {ValidateByHand} // CI has trouble
 import java.nio.charset.*;
 import java.util.*;
 
