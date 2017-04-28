847890c3d44ac8262e52e042bf1b01c850d1ce34
Bruce Eckel <BruceTEckel@gmail.com>
2017-04-28 09:54:06 -0600

    Change tags to {WillNotCompile}

--- housekeeping/OverloadingVarargs2.java
+++ housekeeping/OverloadingVarargs2.java
@@ -2,7 +2,7 @@
 // (c)2017 MindView LLC: see Copyright.txt
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
-// {CompileTimeError} (Will not compile)
+// {WillNotCompile}
 
 public class OverloadingVarargs2 {
   static void f(float i, Character... args) {
--- polymorphism/PrivateOverride2.java
+++ polymorphism/PrivateOverride2.java
@@ -3,7 +3,7 @@
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
 // Detecting a mistaken override using @Override
-// {CompileTimeError} (Will not compile)
+// {WillNotCompile}
 package polymorphism;
 
 public class PrivateOverride2 {
