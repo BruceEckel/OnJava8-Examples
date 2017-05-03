65edbb34be3d7e16bdad5bf4112c6231cc02137a
Bruce Eckel <mindviewinc@gmail.com>
2017-05-03 16:02:28 -0600

    See if this fixes the Appveyor problem

--- lowlevel/AtomicIntegerTest.java
+++ lowlevel/AtomicIntegerTest.java
@@ -2,6 +2,7 @@
 // (c)2017 MindView LLC: see Copyright.txt
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
+// {ValidateByHand} AppVeyor CI has trouble
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.*;
 import java.util.*;
