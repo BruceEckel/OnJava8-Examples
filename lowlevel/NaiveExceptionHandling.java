ec7fed43edf462efc5de4609238978c2eec9e286
Bruce Eckel <mindviewinc@gmail.com>
2017-05-04 10:25:43 -0600

    Remove {ValidateByHand} to test on Travis CI

--- lowlevel/NaiveExceptionHandling.java
+++ lowlevel/NaiveExceptionHandling.java
@@ -2,7 +2,6 @@
 // (c)2017 MindView LLC: see Copyright.txt
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
-// {ValidateByHand}
 // {ThrowsException}
 import java.util.concurrent.*;
 
