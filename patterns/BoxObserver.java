0213d3e6861bf96b916abfbec2c30b6d5ef131d3
Bruce Eckel <mindviewinc@gmail.com>
2017-05-13 17:55:12 -0600

    Only exclude from Travis

--- patterns/BoxObserver.java
+++ patterns/BoxObserver.java
@@ -4,7 +4,7 @@
 // Visit http://OnJava8.com for more book information.
 // Demonstration of Observer pattern using
 // Java's built-in observer classes
-// !!!! CI servers have problems
+// {ExcludeFromTravisCI}
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
