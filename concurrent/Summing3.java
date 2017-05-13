71fd749608565f486d8d95634f3f7fd60a8b6cdc
Bruce Eckel <mindviewinc@gmail.com>
2017-05-13 16:50:26 -0600

    Test CI problem

--- concurrent/Summing3.java
+++ concurrent/Summing3.java
@@ -2,7 +2,7 @@
 // (c)2017 MindView LLC: see Copyright.txt
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
-// {ValidateByHand} CI has trouble
+// !!!! CI has trouble
 import java.util.*;
 
 public class Summing3 {
