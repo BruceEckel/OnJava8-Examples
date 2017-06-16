7df16392cfcedf8ade82b18bd246f34d7ae2154d
Bruce Eckel <BruceTEckel@gmail.com>
2017-06-16 04:03:38 -0600

    From pull request #26

--- housekeeping/Mugs.java
+++ housekeeping/Mugs.java
@@ -8,9 +8,6 @@ class Mug {
   Mug(int marker) {
     System.out.println("Mug(" + marker + ")");
   }
-  void f(int marker) {
-    System.out.println("f(" + marker + ")");
-  }
 }
 
 public class Mugs {
