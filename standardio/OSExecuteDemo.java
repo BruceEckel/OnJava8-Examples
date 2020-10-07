8bfc28c687e7a9653ef96221ece54c94af8988ff
Bruce Eckel <BruceTEckel@gmail.com>
2020-10-07 14:58:30 -0600

    Exclusions to make build work with Java 9

--- standardio/OSExecuteDemo.java
+++ standardio/OSExecuteDemo.java
@@ -3,7 +3,8 @@
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
 // Demonstrates standard I/O redirection
-// {javap -cp build/classes/java/main OSExecuteDemo}
+// javap -cp build/classes/java/main OSExecuteDemo
+// {ExcludeFromGradle}
 import onjava.*;
 
 public class OSExecuteDemo {}
