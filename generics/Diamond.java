5ef986390e83eddc010b8f45d2c18624ba0e94cd
Bruce Eckel <BruceTEckel@gmail.com>
2021-04-18 09:35:27 -0600

    Fixed issue #46

--- generics/Diamond.java
+++ generics/Diamond.java
@@ -3,11 +3,9 @@
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
 
-class Bob {}
-
 public class Diamond<T> {
   public static void main(String[] args) {
-    GenericHolder<Bob> h3 = new GenericHolder<>();
-    h3.set(new Bob());
+    GenericHolder<Automobile> h3 =
+      new GenericHolder<>();
   }
 }
