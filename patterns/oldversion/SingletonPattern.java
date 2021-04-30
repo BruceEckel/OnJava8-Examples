2e4064a546f5926b58e58bfbf83a09c4d2354fa8
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-04-30 11:47:03 +0800

    ok

--- patterns/oldversion/SingletonPattern.java
+++ patterns/oldversion/SingletonPattern.java
@@ -9,35 +9,27 @@ interface Resource {
   int getValue();
   void setValue(int x);
 }
-
 // Since this isn't inherited from a Cloneable
 // base class and cloneability isn't added,
 // making it final prevents cloneability from
 // being added through inheritance. This also
 // implements thread-safe lazy initialization:
 
-final class Singleton {
-  private static final class
-  ResourceImpl implements Resource {
+final class Singleton{
+  private static final class ResourceImpl implements  Resource {
     private int i;
-    private ResourceImpl(int i) {
-      this.i = i;
-    }
-    public synchronized int getValue() {
-      return i;
-    }
-    public synchronized void setValue(int x) {
-      i = x;
-    }
+    private ResourceImpl(int i) { this.i = i; }
+    public synchronized int getValue() { return i; }
+    public synchronized void setValue(int x) { i = x; }
   }
   private static class ResourceHolder {
-    private static Resource resource =
-        new ResourceImpl(47);
+    private static Resource resource = new ResourceImpl(47);
   }
   public static Resource getResource() {
     return ResourceHolder.resource;
   }
 }
+
 
 public class SingletonPattern {
   public static void main(String[] args) {
@@ -46,12 +38,14 @@ public class SingletonPattern {
     Resource s2 = Singleton.getResource();
     s2.setValue(9);
     System.out.println(r.getValue());
+    System.out.println(r == s2); // true
     try {
       // Can't do this: compile-time error.
       // Singleton s3 = (Singleton)s2.clone();
-    } catch(Exception e) {
+    } catch (Exception e) {
       throw new RuntimeException(e);
     }
+
   }
 }
 /* Output:
