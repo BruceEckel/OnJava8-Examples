f69688b838c17e9b4c16b4be27d25e78bd27ca7d
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-04-30 21:53:03 +0800

    ok

--- reuse/Beetle.java
+++ reuse/Beetle.java
@@ -5,12 +5,13 @@
 // The full process of initialization
 
 class Insect {
-  private int i = 9;
-  protected int j;
-  Insect() {
+  private int i = 9; // order3 order4
+  protected int j; // order3
+  Insect() { // order5
     System.out.println("i = " + i + ", j = " + j);
     j = 39;
   }
+  // order1
   private static int x1 =
     printInit("static Insect.x1 initialized");
   static int printInit(String s) {
@@ -20,11 +21,12 @@ class Insect {
 }
 
 public class Beetle extends Insect {
-  private int k = printInit("Beetle.k initialized");
-  public Beetle() {
+  private int k = printInit("Beetle.k initialized"); // order3 order6
+  public Beetle() { // order7
     System.out.println("k = " + k);
     System.out.println("j = " + j);
   }
+  // order2
   private static int x2 =
     printInit("static Beetle.x2 initialized");
   public static void main(String[] args) {
@@ -41,3 +43,5 @@ Beetle.k initialized
 k = 47
 j = 39
 */
+
+
--- reuse/Lisa.java
+++ reuse/Lisa.java
@@ -5,7 +5,7 @@
 // {WillNotCompile}
 
 class Lisa extends Homer {
-  @Override void doh(Milhouse m) {
-    System.out.println("doh(Milhouse)");
-  }
+//  @Override void doh(Milhouse m) {
+//    System.out.println("doh(Milhouse)");
+//  }
 }
